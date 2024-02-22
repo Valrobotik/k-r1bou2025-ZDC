1dab23a3ba70cdcb5ccbc6c0d439632c3ca0db92
zatomos <zatomos@yahoo.com>
2024-02-22 15:36:23 +0100

    WIP detection

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import robotpos_calc as rpc, utils.reset as reset
 
 if __name__ == "__main__" :
-    reset.reset_perspective()
-    #rpc.main(False)
+    #reset.reset_perspective()
+    rpc.main(False)
